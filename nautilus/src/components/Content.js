6fddbc7d5e1c8213e3ee4f6ac5b8c4fc3f18285d
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 15:15:41 -0500

    addded all images

--- nautilus/src/components/Content.js
+++ nautilus/src/components/Content.js
@@ -3,9 +3,12 @@ import bioImg from '../assets/bio_and_beyond.jpeg';
 import wise_sciImg from '../assets/wise_sci_and_engineering.png';
 import tlas from '../assets/tlas.jpeg';
 import abstract from '../assets/abstractions.jpeg';
-import earth from '../assets/earth.jpeg'
-
+import earth from '../assets/earth.jpeg';
+import cosmos from '../assets/cosmos.jpeg';
 import poetry from '../assets/poetry.jpeg';
+import aging from '../assets/aging.png' ;
+import alliance from '../assets/alliance.png' ;
+import maxplanck from '../assets/maxplanck.jpeg' ;
 
 function Content(){
     return(
@@ -16,11 +19,11 @@ function Content(){
                 <div class="image"><img src={abstract} alt="sdf"/> </div>
                 <div class="image"><img src={earth} alt="sdf"/></div>
                 <div class="image"><img src={tlas} alt="sdf"/></div>
-                <div class="image"></div>
+                <div class="image"><img src={cosmos} alt="sdf"/></div>
                 <div class="image"><img src={poetry} alt="sdf"/></div> 
-                <div class="image"></div>
-                <div class="image"></div>
-                <div class="image"></div>
+                <div class="image"><img src={aging} alt="sdf"/></div>
+                <div class="image"><img src={alliance} alt="sdf"/></div>
+                <div class="image"><img src={maxplanck} alt="sdf"/></div>
             </div>
         </React.Fragment>
 );
