82d6d47140241cdb82cd7fe2d4fb503106b501fd
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 14:42:53 -0500

    added divs

---
+++ nautilus/src/components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//import assets from '../assets/aging.png';
+function Content(){
+    return(
+        <React.Fragment>
+            <div class="container">
+                <div class="image1">Hi</div>
+                <div class="image2">Hello </div>
+                <div class="image3">Hi </div>
+                <div class="image4"></div>
+                <div class="image5"></div>
+                <div class="image6"></div>
+                <div class="image7"></div> 
+                <div class="image8"></div>
+                <div class="image"></div>
+                <div class="image10"></div>
+            </div>
+        </React.Fragment>
+);
+}
+export default Content
