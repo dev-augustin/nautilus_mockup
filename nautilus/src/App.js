2eef1d7ab2ef56acdc25f79da2cb43876dccf1f5
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 14:42:25 -0500

    updated App.js

--- nautilus/src/App.js
+++ nautilus/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-//import Content from './components/Content';
+import Content from './components/Content';
  
 function App() {
   return (
     <div className="App">
      
     <Header/>
-    {/* <Content/> */}
+    <Content/>
     
     </div>
   );
