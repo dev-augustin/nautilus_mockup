f6d79e6281a18304c1f4b9c082246328ffa81ede
dev-augustin <dev.augustin00@gmail.com>
2020-03-29 20:21:30 -0500

    updated and aligned text and image positioning

--- nautilus/src/components/Content.js
+++ nautilus/src/components/Content.js
@@ -16,54 +16,54 @@ function Content(){
             <div class="container">
                 <div class="image1" id="img1">
                     <h3 id="biology"> BIOLOGY+BEYOND</h3>
-                    <img src={bioImg} alt="dgdd" height='200px' width="300"/>
+                    <img src={bioImg} alt="dgdd" height='200px' width="300" class="img"/>
                     <h5>Making Sense of the Genome, at Last<br/><em>by Adam Pierre</em></h5>
                 </div>
                 <div class="image2" id="img2">
                     <h3 id="women_sci">
                        Women in Science & Enginerring </h3>
-                    <img src={wise_sciImg} alt="sdfsd" height='200px' width="300"/>
+                    <img src={wise_sciImg} alt="sdfsd" height='200px' width="300" class="img"/>
                     <h5 id="brain">Your Brain is On the Brink of Chaos <br/><em>by Kelly Clancy</em></h5>
                 </div>
                 <div class="image3" id="img3">
                     <h3 id="abstract">QUANTA ABSTRACTIONS</h3>
-                       <img src={abstract} alt="sdf" height='200px' width="300"/>
+                       <img src={abstract} alt="sdf" height='200px' width="300" class="img"/>
                     <h5>Mathematicians Calcualte How <br/>Randomness Creeps In <br/><em>by Marcus Woo</em></h5>
                 </div>
                 <div class="image4">
                 <h3>EARTH</h3>
-                    <img src={earth} alt="dsf" height='200px' width="300"/>
+                    <img src={earth} alt="dsf" height='200px' width="300" class="img"/>
                     <h5>The Deep Time of Walden Pond <br/><em>by Cart Stager</em> </h5>
                 </div>
                 <div class="image5">
                 <h3>THINK LIKE A SCIENTIST</h3>
-                    <img src={tlas} alt="sdfsdfsdf"height='200px' width="300"/>
+                    <img src={tlas} alt="sdfsdfsdf"height='200px' width="300" class="img"/>
                     <h5>The Best Burgr Place Is a Lab<br/><em>by Thomas King</em></h5>
                 </div>
                 <div class="image6">
                 <h3>Cosmos</h3>
-                    <img src={cosmos} alt="sdsdfsdff" height='200px' width="300"/>
+                    <img src={cosmos} alt="sdsdfsdff" height='200px' width="300" class="img"/>
                     <h5>How Much Should Expectation Drive  <br/>Science? <br/><em>
                         by Claudia Gibb</em></h5>
                 </div>
                 <div class="image7">
                 <h3>POETRY IN SCIENCE</h3>
-                    <img src={poetry} alt="sdf"height='200px' width="300"/>
+                    <img src={poetry} alt="sdf"height='200px' width="300" class="img"/>
                     <h5>On Observation and Imagination<br/><em>by Gillian Osborne</em></h5>
                 </div> 
                 <div class="image8">
                 <h3>Aging</h3>
-                    <img src={aging} alt="sdf" height='200px' width="300"/>
+                    <img src={aging} alt="sdf" height='200px' width="300" class="img"/>
                     <h5>Yes, Life in the Fast Lane Kills You <br/><em>by Philip Bell</em></h5>
                 </div>
                 <div class="image9">
                 <h3>SCIENCE PHILANTHROPHY ALLIANCE</h3>
-                    <img src={alliance} alt="sdf" height='200px' width="300"/>
+                    <img src={alliance} alt="sdf" height='200px' width="300" class="img"/>
                     <h5>Taking to the Stars <br/><em>by Science Philanthrophy Alliance</em></h5>
                 </div>
                 <div class="image10">
                 <h3>MPNeuro</h3>
-                    <img src={maxplanck} alt="sdf" height='200px' width="300"/>
+                    <img src={maxplanck} alt="sdf" height='200px' width="300" class="img"/>
                     <h5>Understanding the Brain with the Help of <br/>Artificial Intelligence <br/><em>by Max Planck Institute of Nuerobilogy</em></h5>
                 </div>
             </div>
